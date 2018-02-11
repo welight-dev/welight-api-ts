597c3bdd7192e4be1606d8d5a9a0cfe3fd88adf7
Marcos W. Ferretti <ferretti.spo@gmail.com>
2018-02-11 02:11:50 -0200

    ...

--- lib/welight.ts
+++ lib/welight.ts
@@ -1,4 +1,4 @@
-// Type definitions for [~Welight Platform~] [~1.0.14~]
+// Type definitions for [~Welight Platform~] [~1.0.15~]
 // Project: [~welight-api-ts~]
 // Definitions by: [~MARCOS WILLIAM FERRETTI~] <[~https://github.com/mw-ferretti~]>
 
