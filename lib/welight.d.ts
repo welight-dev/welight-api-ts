fd5de7fb923436a0248547c2a21f7ce4a265251f
Marcos W. Ferretti <ferretti.spo@gmail.com>
2018-02-11 01:01:14 -0200

    ...

--- lib/welight.d.ts
+++ lib/welight.d.ts
@@ -1,2 +1,11 @@
-import * as Welight from './modules/index';
-export { Welight };
+export * from './modules/utils';
+export * from './modules/config';
+export * from './modules/weAuth';
+export * from './modules/weNotify';
+export * from './modules/doador';
+export * from './modules/ong';
+export * from './modules/onu';
+export * from './modules/afiliadora';
+export * from './modules/comparador';
+export * from './modules/suporte';
+export * from './modules/easyImpact';
--- lib/welight.ts
+++ lib/welight.ts
@@ -1,6 +1,15 @@
-// Type definitions for [~Welight Platform~] [~1.0.11~]
+// Type definitions for [~Welight Platform~] [~1.0.12~]
 // Project: [~welight-api-ts~]
 // Definitions by: [~MARCOS WILLIAM FERRETTI~] <[~https://github.com/mw-ferretti~]>
 
-import * as Welight from './modules/index';
-export { Welight };
+export * from './modules/utils';
+export * from './modules/config';
+export * from './modules/weAuth';
+export * from './modules/weNotify';
+export * from './modules/doador';
+export * from './modules/ong';
+export * from './modules/onu';
+export * from './modules/afiliadora';
+export * from './modules/comparador';
+export * from './modules/suporte';
+export * from './modules/easyImpact';
