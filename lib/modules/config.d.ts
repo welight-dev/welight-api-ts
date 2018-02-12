fa57a901f8cf8cf8040f2ead7799359d8e2ab507
Marcos W. Ferretti <ferretti.spo@gmail.com>
2018-02-12 12:41:26 -0200

    ...

--- lib/modules/config.d.ts
+++ lib/modules/config.d.ts
@@ -1,4 +1,6 @@
 export declare class Environment {
+    static env: string;
     static types: any;
     static set(env: string): void;
+    static getDomainSite(name: string, uri?: string): string;
 }
--- lib/modules/config.ts
+++ lib/modules/config.ts
@@ -5,6 +5,8 @@ import * as api from "ts-resource-tastypie";
 
 export class Environment {
 
+    public static env: string = 'local';
+
     public static types: any = {
         local: 'http://127.0.0.1:8001/api/v2/',
         dev: 'https://apidev2.welight.co/api/v2/',
@@ -13,10 +15,41 @@ export class Environment {
 
     public static set(env: string): void {
         if(Environment.types.hasOwnProperty(env)){
+            Environment.env = env;
             api.Tastypie.Provider.add(
                 new api.Tastypie.Provider({name:'welight', url:Environment.types[env]})
             );
             api.Tastypie.Provider.setDefault('welight');
         }
+    }
+
+    public static getDomainSite(name:string, uri?:string): string {
+        let url = ''
+
+        if(Environment.env == 'local'){
+            url = 'http://localhost:4200'+(uri||'');
+        }else if(Environment.env == 'dev'){
+            if(name == 'home'){
+                url = 'https://homedev.welight.co'+(uri||'');
+            }else if(name == 'doador'){
+                url = 'https://donatordev.welight.co'+(uri||'');
+            }else if(name == 'ong'){
+                url = 'https://ongdev.welight.co'+(uri||'');
+            }else if(name == 'easyimpact'){
+                url = 'https://easyimpactdev.welight.co'+(uri||'');
+            }
+        }else if(Environment.env == 'prod'){
+            if(name == 'home'){
+                url = 'https://welight.co'+(uri||'');
+            }else if(name == 'doador'){
+                url = 'https://donator.welight.co'+(uri||'');
+            }else if(name == 'ong'){
+                url = 'https://ong.welight.co'+(uri||'');
+            }else if(name == 'easyimpact'){
+                url = 'https://easyimpact.welight.co'+(uri||'');
+            }
+        }
+
+        return url;
     }
 }
