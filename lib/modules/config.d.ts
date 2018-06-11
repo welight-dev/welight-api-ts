63394773ea6b18781a3687eee4608586b724c71f
Marcos W. Ferretti <ferretti.spo@gmail.com>
2018-06-11 01:38:32 -0300

    ...

--- lib/modules/config.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Environment {
-    static env: string;
-    static types: any;
-    static set(env: string): void;
-    static getDomainSite(name: string, uri?: string): string;
-}
