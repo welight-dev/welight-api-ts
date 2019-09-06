c25582beb6cd1380ef8e736d8e86eb796d282a4f
Marcos W. Ferretti <ferretti.spo@gmail.com>
2019-09-05 23:27:10 -0300

    ...

--- lib/modules/doadorFundoGsQuiz.d.ts
+++ lib/modules/doadorFundoGsQuiz.d.ts
@@ -1,7 +1,13 @@
+export interface QuestionChoice {
+    name: string;
+    value: any;
+    flag: string;
+}
 export declare class QuestionTemplate {
     name: string;
-    choices: any;
-    type: string;
+    choices: Array<QuestionChoice>;
+    form_type: string;
+    evaluation_flag: string;
     dt_created: string;
     dt_updated: string;
     constructor(obj?: any);
--- lib/modules/doadorFundoGsQuiz.ts
+++ lib/modules/doadorFundoGsQuiz.ts
@@ -1,10 +1,17 @@
 // Project: [~welight-api-ts~]
 // Definitions by: [~MARCOS WILLIAM FERRETTI~] <[~https://github.com/mw-ferretti~]>
 
+export interface QuestionChoice {
+    name: string,
+    value: any,
+    flag: string
+}
+
 export class QuestionTemplate {
     public name: string;
-    public choices: any;
-    public type: string;
+    public choices: Array<QuestionChoice>;
+    public form_type: string;
+    public evaluation_flag: string;
     public dt_created: string;
     public dt_updated: string;
 
